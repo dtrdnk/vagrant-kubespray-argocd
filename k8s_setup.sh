34b51220da6819099e406405f08fbdcc10fa363c
dtrdnk <>
2024-03-16 23:22:30 +0300

    Improve installation script

--- k8s_setup.sh
+++ k8s_setup.sh
@@ -2,73 +2,130 @@
 
 kubeconfig="./.vagrant/provisioners/ansible/inventory/artifacts/admin.conf"
 
+# This function calling with one arg, which contain application name
+function get_application_version() {
+  local version
+  version=$(yq eval ".applications.[] | select(.name == \"$1\") .targetRevision" infra/home-lab-values.yaml)
+
+  if [[ "${version}" == "" ]]; then
+    echo "Fatal! There is no version for $1 application in infra/home-lab-values.yaml. Exit." >&2
+    exit 1
+  else
+    echo "${version}"
+  fi
+}
+
+# Check is the "yq" "kubectl" "helm" already installed on host
+function check_is_cli_tools_installed() {
+  components_array=("yq" "kubectl" "helm")
+  for i in "${components_array[@]}"; do
+    command -v "${i}" >/dev/null 2>&1 ||
+      {
+        echo "${i} is required, but it's not installed. Aborting." >&2
+        exit 1
+      }
+  done
+}
+
+# home-lab-values.yaml is single source of truth for helm installation. The file must be exist.
+function check_is_values_file_exist() {
+  if ! [[ -f "./infra/home-lab-values.yaml" ]]; then
+    echo "Fatal! There is no values file home-lab-values.yaml in ${PWD}/infra. Exit." >&2
+    exit 1
+  fi
+}
+
+# admin.conf like ~/.kube/config file. Must be appear after kubespray successfully running.
 function check_is_kubeconfig_exist() {
   if ! [[ -f "${kubeconfig}" ]]; then
-    echo "Fatal! There is no .vagrant/provisioners/ansible/inventory/artifacts/admin.conf in ${PWD}. Exit." >&2; exit 1;
+    echo "Fatal! There is no .vagrant/provisioners/ansible/inventory/artifacts/admin.conf in ${PWD}. Exit." >&2
+    exit 1
   fi
 }
 
 function add_and_update_helm_repos() {
-  helm --kubeconfig "${kubeconfig}" repo add argo https://argoproj.github.io/argo-helm;
-  helm --kubeconfig "${kubeconfig}" repo add jetstack https://charts.jetstack.io;
-  helm --kubeconfig "${kubeconfig}" repo add istio https://istio-release.storage.googleapis.com/charts;
-  helm --kubeconfig "${kubeconfig}" repo add metallb https://metallb.github.io/metallb;
-  helm --kubeconfig "${kubeconfig}" repo update argo jetstack istio;
+  helm --kubeconfig "${kubeconfig}" repo add argo https://argoproj.github.io/argo-helm
+  helm --kubeconfig "${kubeconfig}" repo add jetstack https://charts.jetstack.io
+  helm --kubeconfig "${kubeconfig}" repo add istio https://istio-release.storage.googleapis.com/charts
+  helm --kubeconfig "${kubeconfig}" repo add metallb https://metallb.github.io/metallb
+  helm --kubeconfig "${kubeconfig}" repo update argo jetstack istio
 }
 
+# Metallb app allow you using service aka loadBalancer with ip in the same network in L2 mode
 function upgrade_install_metallb() {
-  echo "Installing metallb. Please wait...";
+  echo "Installing metallb. Please wait..."
   helm --kubeconfig "${kubeconfig}" \
-  upgrade --install metallb --create-namespace \
-  --namespace metallb-system --version 0.14.3 \
-  metallb/metallb
+    upgrade --install metallb --create-namespace \
+    --namespace metallb-system --version "$(get_application_version 'metallb')" \
+    metallb/metallb
 }
 
+# Cert-manager needs for self-signed certs for istio service mesh
 function upgrade_install_cert_manager() {
-  echo "Installing cert-manager. Please wait...";
+  echo "Installing cert-manager. Please wait..."
   helm --kubeconfig "${kubeconfig}" \
-  upgrade --install cert-manager --create-namespace \
-  jetstack/cert-manager -f ./infra/values/cert-manager/cert-manager.yaml \
-  --namespace cert-manager --version v1.14.3  || \
-  { echo "Failure of argocd installation. Aborting."; exit 1; }
+    upgrade --install cert-manager --create-namespace \
+    jetstack/cert-manager -f ./infra/values/cert-manager/cert-manager.yaml \
+    --namespace cert-manager --version "$(get_application_version 'cert-manager')" ||
+    {
+      echo "Failure of argocd installation. Aborting."
+      exit 1
+    }
 }
 
+# Istio allow you create service mesh and with specific entrypoint
 function upgrade_install_istio() {
-  echo "Installing istio components. Please wait...";
+  echo "Installing istio components. Please wait..."
   helm --kubeconfig "${kubeconfig}" upgrade --install istio-base \
-    istio/base -n istio-system --create-namespace --version 1.20.3 || \
-    { echo "Failure of istio-base installation. Aborting."; exit 1; }
+    istio/base -n istio-system --create-namespace --version "$(get_application_version 'istio-base')" ||
+    {
+      echo "Failure of istio-base installation. Aborting."
+      exit 1
+    }
   helm --kubeconfig "${kubeconfig}" upgrade --install istiod \
-    istio/istiod -n istio-system --version 1.20.3 || \
-    { echo "Failure of Istiod installation. Aborting."; exit 1; }
+    istio/istiod -n istio-system --version "$(get_application_version 'istiod')" ||
+    {
+      echo "Failure of Istiod installation. Aborting."
+      exit 1
+    }
   helm --kubeconfig "${kubeconfig}" upgrade --install istio-ingressgateway \
-    istio/gateway -n istio-system --version 1.20.3 || \
-    { echo "Failure of istio-ingressgateway installation. Aborting."; exit 1; }
+    istio/gateway -n istio-system --version 1.20.3 ||
+    {
+      echo "Failure of istio-ingressgateway installation. Aborting."
+      exit 1
+    }
 }
 
+# ArgoCD control of installations in k8s cluster
 function upgrade_install_argocd() {
-  echo "Installing argocd. Please wait...";
+  echo "Installing argocd. Please wait..."
   helm --kubeconfig "${kubeconfig}" upgrade \
     --install argocd argo/argo-cd --wait \
     --create-namespace -n argocd \
     -f ./infra/values/argocd/argocd.yaml \
-    --version 6.6.0 || { echo "Failure of argocd installation. Aborting."; exit 1; }
+    --version "$(get_application_version 'argocd')" || {
+    echo "Failure of argocd installation. Aborting."
+    exit 1
+  }
 }
 
+# There is some extra manifest, which needs to apply for correct work istio, metallb and cert-manager apps
 function apply_extra_manifests() {
-  echo "Apply extra manifests. Please wait...";
+  echo "Apply extra manifests. Please wait..."
   helm template --show-only templates/extra-manifests.yaml extras ./infra \
-  -f infra/home-lab-values.yaml | kubectl --kubeconfig "${kubeconfig}" apply -f -
+    -f infra/home-lab-values.yaml | kubectl --kubeconfig "${kubeconfig}" apply -f -
 }
 
 function main() {
-    check_is_kubeconfig_exist
-    upgrade_install_metallb
-    add_and_update_helm_repos
-    upgrade_install_cert_manager
-    upgrade_install_istio
-    upgrade_install_argocd
-    apply_extra_manifests
+  check_is_cli_tools_installed
+  check_is_values_file_exist
+  check_is_kubeconfig_exist
+  upgrade_install_metallb
+  add_and_update_helm_repos
+  upgrade_install_cert_manager
+  upgrade_install_istio
+  upgrade_install_argocd
+  apply_extra_manifests
 }
 
 main
