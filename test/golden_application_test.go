6042ab24d105ff20c47ffe85a835d958383cf621
dtrdnk <>
2024-03-17 21:13:09 +0300

    Refactoring avp params. Add inlineValues golden file

--- test/golden_application_test.go
+++ test/golden_application_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-func TestApplicationTemplate(t *testing.T) {
+func TestApplicationCommonTemplate(t *testing.T) {
 	t.Parallel()
 
 	helmChartPah, err := filepath.Abs("../infra")
@@ -31,6 +31,29 @@ func TestApplicationTemplate(t *testing.T) {
 			"applications[0].ignoreDifferences[0].kind":            "ValidatingWebhookConfiguration",
 			"applications[0].ignoreDifferences[0].name":            "istiod-default-validator",
 			"applications[0].ignoreDifferences[0].jsonPointers[0]": "/webhooks/0/failurePolicy",
+		},
+	})
+}
+
+func TestApplicationAdvancedTemplate(t *testing.T) {
+	t.Parallel()
+
+	helmChartPah, err := filepath.Abs("../infra")
+	require.NoError(t, err)
+
+	suite.Run(t, &utils.TemplateGoldenTest{
+		ChartPath:      helmChartPah,
+		Release:        "applications",
+		GoldenFileName: "application-helmInlineAVPValue",
+		Templates:      []string{"templates/application.yaml"},
+
+		SetValues: map[string]string{
+			"applications[0].name":               "cert-manager",
+			"applications[0].namespace":          "cert-manager",
+			"applications[0].chart":              "cert-manager",
+			"applications[0].repoURL":            "https://charts.jetstack.io",
+			"applications[0].targetRevision":     "v1.14.3",
+			"applications[0].helmInlineAVPValue": "true",
 		},
 	})
 }
